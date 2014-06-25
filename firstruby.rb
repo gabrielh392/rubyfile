e194ffc3478a6da77f85865480583bdce1b62240
Gabriel <gabrielh@activestate.com>
2014-06-25 16:15:38 -0700

    Added a square method with some user input! Hope that's ok Kantar.

--- firstruby.rb
+++ firstruby.rb
@@ -1,2 +1,13 @@
+def square(x)
+    x * x
+end
+
+puts "Enter a number to square!"
+number = gets.chomp!
+squared = square(number.to_i)
+
+puts "The result is: #{squared}!"
+
 puts "First ruby file."
 puts "Anyways, goodbye, world!"
+
